f1682dc5e4aa3dba8dff8010f774c5f2606d330b
Mitchell Paulus <paulusm14@gmail.com>
2021-09-19 20:57:53 -0500

    Add 3P-C model, option to print model coordinates

--- cpmodel/Program.cs
+++ cpmodel/Program.cs
@@ -13,11 +13,19 @@ namespace cpmodel
         {
             string filename = "";
             string type = "4";
+
+            bool printModelCoords = false;
+
             for (int i = 0; i < args.Length; i++)
             {
                 if (args[i] == "-p")
                 {
                     type = args[i + 1];
+                    i++;
+                }
+                else if (args[i] == "-c")
+                {
+                    printModelCoords = true;
                 }
                 else
                 {
@@ -25,15 +33,30 @@ namespace cpmodel
                 }
             }
 
-            string[] data = File.ReadAllLines(Path.GetFullPath(filename), Encoding.UTF8);
+            IEnumerable<string> data;
+            if (filename == "-")
+            {
+                data = Console.In.ReadToEnd().SplitLines();
+            }
+            else
+            {
+                data = File.ReadAllLines(Path.GetFullPath(filename), Encoding.UTF8);
+            }
+
 
             XTransformer transformer = new();
 
-            var pointData = data.Select(s =>
+            var pointData = data.Select((s, idx) =>
             {
                 var split = s.Split(null);
 
-                return new Point(double.Parse(split[0]), double.Parse(split[1]));
+                if (!double.TryParse(split[0], out double x))
+                {
+                    Console.Error.WriteLine($"Could not parse x value '{split[0]}' on line {idx}.");
+                    throw new InvalidDataException("");
+                }
+
+                return new Point(x, double.Parse(split[1]));
             }).ToList();
 
             ModelRunner runner = new();
@@ -54,7 +77,29 @@ namespace cpmodel
                     Console.WriteLine(outputsCoeff);
                 }
             }
-            else
+            else if (type == "3c_new")
+            {
+                RegressionOutputs outputs = runner.Run3PCNew(pointData);
+
+                if (printModelCoords)
+                {
+                    var minXValue = Math.Floor(pointData.Select(point => point.X).Min());
+                    var maxXValue = Math.Ceiling(pointData.Select(point => point.X).Max());
+
+                    Console.WriteLine($"{minXValue}\t{outputs.Coeffs[0]}");
+                    Console.WriteLine($"{outputs.Coeffs[2]}\t{outputs.Coeffs[0]}");
+                    Console.WriteLine($"{maxXValue}\t{outputs.Coeffs[0] + outputs.Coeffs[1] * (maxXValue - outputs.Coeffs[2])}");
+                }
+                else
+                {
+                    foreach (double outputsCoeff in outputs.Coeffs)
+                    {
+                        Console.WriteLine(outputsCoeff);
+                    }
+                }
+
+            }
+            else if (type == "4")
             {
                 (double cp, RegressionOutputs regOutputs) output = runner.Run4P(pointData);
 
@@ -62,6 +107,11 @@ namespace cpmodel
                 Console.WriteLine(output.regOutputs.Coeffs[1]);
                 Console.WriteLine(output.regOutputs.Coeffs[2]);
                 Console.WriteLine(output.cp);
+            }
+            else
+            {
+                Console.WriteLine($"The model type '{type}' has not been implemented.");
+                return;
             }
         }
     }
@@ -195,8 +245,73 @@ namespace cpmodel
             };
             return outputs;
         }
-    }
 
+        public RegressionOutputs Run3PCNew(List<Point> points)
+        {
+            IOrderedEnumerable<Point> sortedPoints = points.OrderBy(point => point.X);
+
+            List<double> ys = sortedPoints.Select(point => point.Y).ToList();
+            List<double> xs = sortedPoints.Select(point => point.X).ToList();
+
+            int n = points.Count;
+            double sumY = ys.Sum();
+
+            double sse = double.MaxValue;
+
+            double bestb0 = 0;
+            double bestb1 = 0;
+            double bestb2 = 0;
+
+            for (int m = 1; m < n - 1; m++)
+            {
+                double b0 = ys.Take(m).Average();
+
+                List<double> highXs = xs.Skip(m).ToList();
+                List<double> highYs = ys.Skip(m).ToList();
+
+                double sumXHigh = highXs.Sum();
+                double sumYHigh = highYs.Sum();
+                double sumXYHigh = highXs.Zip(highYs).Select((pair) => pair.First * pair.Second).Sum();
+                double sumX2High = highXs.Select(d => d * d).Sum();
+                int ng = n - m;
+
+                // Equation 18
+                double b1 = (ng * sumXYHigh -  sumXHigh * sumYHigh) / (ng * sumX2High - sumXHigh * sumXHigh);
+
+                // Equation 20
+                double numerator = (n - ng) * sumXYHigh * sumXHigh + sumX2High * (ng * sumY - n * sumYHigh) +
+                                   (sumXHigh * sumXHigh) * (sumYHigh - sumY);
+
+                // Equation 21
+                double denominator = (n - ng) * (ng * sumXYHigh - sumXHigh * sumYHigh);
+
+                double b2 = numerator / denominator;
+
+                IEnumerable<double> tempPortion = xs.Select(x => Math.Max(x - b2, 0));
+                List<double> predictions = tempPortion.Select(d => b0 + b1 * d).ToList();
+
+                List<double> residuals = new();
+                for (int i = 0; i < n; i++) residuals.Add(ys[i] - predictions[i]);
+
+                double newSse = residuals.Select(r => r * r).Sum();
+
+                if (newSse < sse)
+                {
+                    sse = newSse;
+                    bestb0 = b0;
+                    bestb1 = b1;
+                    bestb2 = b2;
+                }
+            }
+
+            RegressionOutputs outputs = new()
+            {
+                Coeffs = new[] { bestb0, bestb1, bestb2 }
+            };
+            return outputs;
+        }
+
+    }
 
     public class RangeBuilder
     {
@@ -236,6 +351,19 @@ namespace cpmodel
             }
 
             return array;
+        }
+        public static List<string> SplitLines(this string input)
+        {
+            List<string> output = new List<string>();
+            using (StringReader sr = new StringReader(input)) {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    output.Add(line);
+                }
+            }
+
+            return output;
         }
     }
 
